fff90eff2439d6ddbca05f701f8dc9647b15bdf5
eunae kim <elkim0118@gmail.com>
2026-01-04 04:45:39 +0900

    fix: add missing AccessLevel import and resolve entity compilation errors

--- Matcha/backend/src/main/java/com/matchaworld/backend/domain/Certification.java
+++ Matcha/backend/src/main/java/com/matchaworld/backend/domain/Certification.java
@@ -23,6 +23,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.AccessLevel;
 
 @Entity
 @Getter
--- Matcha/backend/src/main/java/com/matchaworld/backend/domain/User.java
+++ Matcha/backend/src/main/java/com/matchaworld/backend/domain/User.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
+import lombok.AccessLevel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
